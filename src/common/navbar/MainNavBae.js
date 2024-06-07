886f2c40b9d48fd6b26f867c0d4d0ec20ef0a94a
Facundo VARAS <102789450+cundovar@users.noreply.github.com>
2024-06-07 17:12:43 +0200

    first commt

--- src/common/navbar/MainNavBae.js
+++ src/common/navbar/MainNavBae.js
@@ -41,7 +41,7 @@ return (
     {Array.isArray(menus) && menus.map(menu => (
         <div key={menu.id} className="">
             <div className="" onMouseEnter={handleMouseEnter} >
-            <h3 onMouseEnter={() => handleMouseEnter(menu)}>{menu.name}</h3>
+            <h3 onMouseEnter={() => handleMouseEnter(menu)}className="cursor-pointer" >{menu.name}</h3>
 
             </div>
                 </div>
@@ -51,21 +51,21 @@ return (
         {Array.isArray(menus) && menus.map(menu => (
               hoveredMenu && hoveredMenu.id === menu.id && (
             menu.submenus && menu.submenus.length > 0 && (
-                        <div className={`${ishover ? "visible w-96 border  " : "hidden" }`}>
+                        <div className={`${ishover ? "visible border  " : "hidden" }`}>
                             {menu.submenus.map(subMenu => (
                                 
                                 subMenu.children && subMenu.children.length > 0 && (
-                                    <div key={subMenu.id} className=" w-full bg-orange-400 ">
-                                        <h4>{subMenu.name}</h4>
-                                        <div style={{ paddingLeft: '20px' }}>
+                                    <div key={subMenu.id} className=" w-[50rem] bg-orange-400 ">
+                                        <h4 className="border-b">{subMenu.name}</h4>
+                                        <div style={{ paddingLeft: '20px' }}className="flex-container" >
                                             {subMenu.children.map(child => (
                                             child.menuContents && child.menuContents.length > 0 ? (
-                                                <div key={child.id}>
+                                                <div key={child.id} className="flex-item">
                                                      <Link to={`/article/${child.menuContents[0].split('/').pop()}`}>
                                                     <h5>{child.name}</h5>
                                                     </Link>
                                                 </div>
-                                        ):(<h5> te : {child.name}</h5>)
+                                        ):(<h5> vide : {child.name}</h5>)
                                             ))}
                                         </div>
                                     </div>
--- src/page/articleId.js
+++ src/page/articleId.js
@@ -1,68 +1,76 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import Prism from 'prismjs';
-import 'prismjs/themes/prism.css'; // Ou un autre thème si vous préférez
-import 'prismjs/components/prism-javascript'; // Importez les langages que vous souhaitez utiliser
+import Prism from "prismjs";
+import "prismjs/themes/prism.css"; // Ou un autre thème si vous préférez
+import "prismjs/components/prism-javascript"; // Importez les langages que vous souhaitez utiliser
 import SearchBar from "../common/navbar/chearchBar";
 import { useSearchResult } from "../common/navbar/resultChearchContext";
 // Ajoutez d'autres langages ici si nécessaire
 
-
-
-
-
-const ArticleId=()=>{
-    const { results } = useSearchResult();
-    const {id}=useParams()
-    const [content,setContent]=useState(null)
-    const [error, setError] = useState(null);
-    useEffect(()=>{
-        axios.get(`http://localhost:8000/api/menu_contents/${id}`)
-        .then(response => setContent(response.data))
-        .catch(error => console.error('Error fetching menus:', error));
-}, [id]);
-useEffect(() => {
+const ArticleId = () => {
+  const { results } = useSearchResult();
+  const { id } = useParams();
+  const [content, setContent] = useState(null);
+  const [error, setError] = useState(null);
+  useEffect(() => {
+    axios
+      .get(`http://localhost:8000/api/menu_contents/${id}`)
+      .then((response) => setContent(response.data))
+      .catch((error) => console.error("Error fetching menus:", error));
+  }, [id]);
+  useEffect(() => {
     if (content) {
-        Prism.highlightAll(); // Met en évidence tous les éléments de code après le rendu du contenu
+      Prism.highlightAll(); // Met en évidence tous les éléments de code après le rendu du contenu
     }
-}, [content]);
+  }, [content]);
 
-
-console.log("data",content)
-if (!content) {
+  console.log("data", content);
+  if (!content) {
     return <div>Loading...</div>;
   }
 
-
-
-
-    return(
-<>
-
-{results.map((result, index) => (
+  return (
+    <>
+      {results.map((result, index) => (
         <div key={index}>
           <h2>{result.title}</h2>
           <p>{result.content}</p>
         </div>
       ))}
-    
-             
-                    <h2>{content.title} </h2>
-                    <p>{content.para} </p>
-                    <pre className="language-js">
-<code>
-                    {content.content}
 
-</code>
-                    </pre>
-              
-                    <p>{content.para1} </p>
-         
+      <h2>{content.title} </h2>
+      <div className="w-1/2 text-justify">
+        <p className="text-xl">{content.para} </p>
+      </div>
+      <pre className="language-js">
+        <code>{content.content}</code>
+      </pre>
+      <div className="w-1/2 text-justify">
+        <p className="text-xl">{content.para1} </p>
+      </div>
+      <div className="flex w-full">
+        <pre classname={` language-js ${content.image ? "col 6 language-js" : "language-js"}`}>
+          <code className="language-js">{content.content1}</code>
+        </pre>
+        {content.image && (
+          <div classname="col-6">
+            {content.image ? (
+              <img
+                src={`http://localhost:8000/images/admin/${content.image}`}
+                alt={content.title}
+              />
+            ) : (
+              <p>loading...</p>
+            )}
+          </div>
 
-</>
+          
+        )}
+      </div>
+      
+    </>
+  );
+};
 
-    )
-}
-
-export default ArticleId
+export default ArticleId;
